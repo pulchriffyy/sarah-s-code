45dbca6482a06464548a75279c4ea963a8ca2e3d
pulchriffyy <tayyabsarah71@gmail.com>
2025-08-12 13:27:25 +0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,28 @@
+print ("Welcome to Python!")
+name = "Sarah"
+age = 10
+weight = 38.5
+is_student = True
+last_name = " Syeda"
+print ("name :", name)
+print (type (name))
+print ("last_name :", last_name)
+print (type (last_name))
+print ("age :", age)
+print (type (age))
+print ("weight :", weight)
+print (type (weight))
+print ("is_student :", is_student)
+print (type (is_student))
+weight = int (weight)
+print ("weight :", weight)
+print (type (weight))
+
+num1 = 3
+num2 = 2760
+print ("result :", num1 + num2)
+print ("result :", num1 - num2)
+print ("result :", num1 * num2)
+print ("result :", num1 // num2)
+print ("exponent :", num1 ** 2)
+print (name + last_name)
