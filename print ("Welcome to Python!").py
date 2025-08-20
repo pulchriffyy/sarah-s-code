fb170203ec9349ba42ddcf27e64384e39dd70f98
pulchriffyy <tayyabsarah71@gmail.com>
2025-08-20 09:38:54 +0700

    Add files via upload

---
+++ print ("Welcome to Python!").py
@@ -0,0 +1,19 @@
+print ("Welcome to Python!")
+name = "Sarah"
+age = 10
+weight = 40.4
+grade = "6th grade and im going to 7th grade"
+last_name = " Syeda"
+print ("Hi people! my name is :", name)
+print (type (name))
+print ("And my last name is::", last_name)
+print (type (last_name))
+print ("I'm :", age)
+print (type (age))
+print ("My weight is :", weight)
+print (type (weight))
+print (" My grade is :", grade)
+print (type (grade))
+weight = int (weight)
+
+print (type (weight))
