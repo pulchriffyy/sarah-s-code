e85d2860eb61afb1d8eea4dc8b805b8152e9b270
pulchriffyy <tayyabsarah71@gmail.com>
2025-08-28 16:00:14 +0700

    Add files via upload

---
+++ Class 8tgh 28 .py
@@ -0,0 +1,7 @@
+def absolute_value(num):
+    if (num >= 0):
+        return(num)
+    else:
+        return(-num)
+print("absolute value of the number is", absolute_value(4))
+print("the absolute value for the negative number is:", absolute_value(-4))
