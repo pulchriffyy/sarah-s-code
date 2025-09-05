58ab4de0722378843bf4bac1cb2a049c69632303
pulchriffyy <tayyabsarah71@gmail.com>
2025-09-05 13:59:50 +0700

    Add files via upload

---
+++ Dictionary .py
@@ -0,0 +1,10 @@
+my_dict = {}
+my_dict = {1: 'cloud', 2: 'dragonfly'}
+print(my_dict)
+my_dict = {'name' : 'connie', 1: [2,4,3]}
+print(my_dict)
+my_dict = {'name': 'olivia', 'age': 1400303003}
+print(my_dict['name'])
+my_dict['adress'] = "suburb"
+my_dict.pop("name")
+print(my_dict)
