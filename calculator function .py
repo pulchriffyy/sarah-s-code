841cc5582423c164ca33ea972d3c44ba83e14384
pulchriffyy <tayyabsarah71@gmail.com>
2025-09-07 11:27:30 +0700

    Add files via upload

---
+++ calculator function .py
@@ -0,0 +1,8 @@
+number = int(input("Enter the number to minus!"))
+print ("number::", number )
+secondnumber = int(input("Enter another."))
+print ("number::", number )
+result = number - secondnumber
+result = number + secondnumber
+result = number * secondnumber
+print("results minus")
