2ccd8f6ac5576b83e5bb030e49276e1637c2a6da
pulchriffyy <tayyabsarah71@gmail.com>
2025-09-15 10:37:45 +0700

    Add files via upload

--- Circles OOP HMW .py
+++ Circles OOP HMW .py
@@ -1,6 +1,9 @@
+import math
 class Circle:
     def __init__(self, r):
         self.radius = r
 new_circle = Circle(5) 
-
+def calculate_area(self):
+     math.pi * (self.radius ** 2)
 print("Radius of circle:", new_circle.radius)
+print("Area of circle:", math.pi)
