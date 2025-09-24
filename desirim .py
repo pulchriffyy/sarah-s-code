4f8c0160f9d104e4cfed0bf442e176b7826334ee
⇢ ˗ˏˋ  klopper-fish ˎˊ˗ <tayyabsarah71@gmail.com>
2025-09-24 12:22:55 +0700

    Add files via upload

---
+++ desirim .py
@@ -0,0 +1,6 @@
+num=int(input("enter a number"))
+# number inputted will start code.
+length =len(str(num))
+print(length)
+#  the length of the number is powered by the number inputted.
+#  ie 135, 3 digits, 1(1) 3 (2) 5 (3) = 1 + 9 + 125 = 135.
