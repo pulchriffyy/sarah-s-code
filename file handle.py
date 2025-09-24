94b6dd90cf9fd2640f01d073fe82061fb318a1a8
⇢ ˗ˏˋ  klopper-fish ˎˊ˗ <tayyabsarah71@gmail.com>
2025-09-24 13:18:44 +0700

    Add files via upload

--- file handle.py
+++ file handle.py
@@ -17,3 +17,12 @@ file2.close()
 file1 = open('codingal.txt', 'r')
 print(file1.read())
 file1.close()
+
+file1 = open('codingal.txt', 'r')
+file2 = open('cdnglw.txt', 'w')
+for line in file1.readlines():
+    if not(line.startswith("coding")):
+        print(line)
+        file2.write(line)
+file1.close()
+file2.close()
---
+++ fileread.py
@@ -0,0 +1,8 @@
+file1 = open('codingal.txt', 'r')
+file2 = open('cdnglw.txt', 'w')
+for line in file1.readlines():
+    if not(line.startswith("coding")):
+        print(line)
+        file2.write(line)
+file1.close()
+file2.close()
