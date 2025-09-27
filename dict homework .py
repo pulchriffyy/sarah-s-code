f25896fecac4a8a3b9e13edb114ecd79d08fb399
⇢ ˗ˏˋ  klopper-fish ˎˊ˗ <tayyabsarah71@gmail.com>
2025-09-27 11:26:49 +0700

    Add files via upload

--- dict homework .py
+++ dict homework .py
@@ -1,5 +1,12 @@
 my_dict = {}
-word = (input("Enter the word!"))
-print ("number::", word )
-my_dict = {1: word}
-print(my_dict)
+word = int(input("Enter a number, 1 - 3 and get a prize"))
+my_dict = {1:'200 million ', 2:'89 cats', 3:'1 trillion baht'}
+if word == 1 :
+    print(my_dict[1])
+elif word == 2 :
+    print(my_dict[2])
+else:
+    print(my_dict[3])
+if word > 3:
+    print("Please do number 1,2,3. Not above")
+    
