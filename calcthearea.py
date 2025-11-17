5630472aacdc3fd46b9e9a77fc15c54243b43a14
⇢ ˗ˏˋ  klopper-fish ˎˊ˗ <tayyabsarah71@gmail.com>
2025-11-17 21:06:12 +0700

    Update calcthearea.py

--- calcthearea.py
+++ calcthearea.py
@@ -1,11 +1,12 @@
 def sq():
-          a = float(input("enter the length of your square!   : "))
-          b = float(input("enter the length of your square!   : "))
+          a = float(input("enter the length of your rectangle!   : "))
+          b = float(input("enter the length of your rectangle!   : "))
           print("the area of the given numebrs is:",a*b)
-ask = input("do you want to calculate the area of a square? y/n")
+ask = input("do you want to calculate the area of a rectangle? y/n")
 if ask == "y":
         sq()
 elif ask == 'n':
         print("okay see you next time!")
 else:
+
         print("please enter a valid input. ")
