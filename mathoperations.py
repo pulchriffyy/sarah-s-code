2d22f57de6a06d3d4439d02f4b29143b15ba81bb
⇢ ˗ˏˋ  klopper-fish ˎˊ˗ <tayyabsarah71@gmail.com>
2025-11-27 15:01:33 +0700

    Add files via upload

---
+++ mathoperations.py
@@ -0,0 +1,7 @@
+import math
+print("floor & ceil value of 42.64 is\n",math.floor(42.64),"--", math.ceil(42.64))
+x = 5
+y = -9
+print("the absolute value of -9 is: ", math.fabs(y))
+print("gcd of 15 and 25 is: ", math.gcd(15,25))
+print("the copied sign of -9 is: ",math.copysign(x,y))
